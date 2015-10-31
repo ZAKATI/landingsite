db91494ceb9f1916752df82caf3b3b583910f41f
domino <elwadiamine@gmail.com>
2015-10-31 23:56:50 +0000

    slide on first page

--- js/init.js
+++ js/init.js
@@ -2,6 +2,9 @@
   $(function(){
 
     $('.button-collapse').sideNav();
-
+    $('.slider').slider({
+      full_width: true,
+      indicators:false
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
